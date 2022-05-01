f39ea6434fffd84baf0574b39a862f0c5b07c1de
气泡缓释胶囊 <japanztp@gmail.com>
2022-05-01 17:47:21 +0800

    Landscape

--- 2D RollBall/Assets/Script/BehaviourSystem/Ball/Controller.cs
+++ 2D RollBall/Assets/Script/BehaviourSystem/Ball/Controller.cs
@@ -4,34 +4,33 @@ using System.Collections.Generic;
 using System.Xml.Schema;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D Rigid2D;
-    public bool UseKeyboard = false;
+    // public bool UseKeyboard = false;
     public int pow;
 
     private void Awake()
     {
         Rigid2D = gameObject.GetComponent<Rigidbody2D>();
     }
+    void Start()
+    {
+        if(AttitudeSensor.current != null)
+            InputSystem.EnableDevice(Accelerometer.current);
+    }
 
     private void FixedUpdate()
     {
-        Vector2 acc2D = new Vector2();
-        if (UseKeyboard)
-        {
-            // acc2D = new Vector2(horizontal * speed, vertical * speed);
-            // Rigid2D.AddForce(acc2D);
-        }
-        else
-        {
-            Vector3 acc = Input.acceleration;
-            var vertical = math.pow(acc.x, pow);
-            var horizontal = math.pow(acc.y, pow);
-            acc2D = new Vector2(acc.x, acc.y);
-            Rigid2D.AddForce(acc2D);
-        }
+        
+        Vector3 acc = Accelerometer.current.acceleration.ReadValue();
+        // var vertical = math.pow(acc.x, pow);
+        // var horizontal = math.pow(acc.y, pow);
+        var acc2D = new Vector2(acc.x, acc.y) * speed;
+        Rigid2D.AddForce(acc2D);
+        
     }
 }
